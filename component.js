a8ecc6cfb98f459ae179f5581519750c33224a74
Merrick Christensen <merrick.christensen@gmail.com>
2012-11-05 12:39:01 -0700

    Compiles hadnlebars templates.

--- component.js
+++ component.js
@@ -8,8 +8,19 @@ var path = require('path');
 module.exports = {
   compile: function(templates, destination, stylesheets, compiler) {
     var ComponentCompiler = require(util.format('./lib/compilers/%s/Compiler', compiler));
-    var ComponentWriter = require(util.format('./lib/compilers/%s/Writer', compiler));
-    compiler = new ComponentCompiler(path.normalize(templates), path.normalize(stylesheets));
+    var ComponentWriter = require('./lib/ComponentWriter');
+    
+    
+    /**
+     * If stylesheets is passed in, normalize it.
+     */
+    if (stylesheets) {
+      stylesheets = path.normalize(stylesheets)
+    }
+    
+    compiler = new ComponentCompiler(path.normalize(templates), stylesheets, {
+      destination: path.normalize(destination)
+    });
     var writer = new ComponentWriter(path.normalize(destination));
     
     compiler.pipe(writer);
